903078b408812958fa816f0e0c2e27ff8b89bfae
Alex Gustafsson <alex-gustafsson@live.se>
2026-02-15 16:48:58 +0100

    Build and embed go cli

--- Steward/IndexView.swift
+++ Steward/IndexView.swift
@@ -15,8 +15,28 @@ struct IndexView: View {
                 savePanel.begin { (result) in
                     if result == .OK {
                         showIndexProgressSheet = true
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                            self.showIndexProgressSheet = false
+                        
+                        let helperURL = Bundle.main.bundleURL
+                            .appendingPathComponent("Contents/MacOS/StewardTool")
+
+                        let process = Process()
+                        process.executableURL = helperURL
+                        process.arguments = ["--help"]
+                        
+                        let pipe = Pipe()
+                        process.standardOutput = pipe
+                        process.standardError = pipe
+                        
+                        do {
+                            try process.run()
+                            process.waitUntilExit()
+                            
+                            let data = pipe.fileHandleForReading.readDataToEndOfFile()
+                                if let output = String(data: data, encoding: .utf8) {
+                                    print(output)
+                                }
+                        } catch {
+                            print("Failed \(error)")
                         }
                     }
                 }
