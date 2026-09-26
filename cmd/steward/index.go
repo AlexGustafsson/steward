0bc2f05d0b55b4bf11bf3a4cbbb9b7d6e28c6d99
Alex Gustafsson <alex-gustafsson@live.se>
2026-09-26 09:57:12 +0200

    Fix returned error syntax

--- cmd/steward/index.go
+++ cmd/steward/index.go
@@ -13,7 +13,7 @@ func IndexAction(ctx context.Context, cmd *cli.Command) error {
 	args := cmd.Args()
 	if args.Len() == 0 {
 		_ = cli.ShowAppHelp(cmd)
-		return ExitErrorf(1, "")
+		return ErrExit{Code: 1}
 	}
 
 	encoder := json.NewEncoder(os.Stdout)
